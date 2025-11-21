304c8985fbf4bd9f50178814a24eaeb5fc89665f
Kanit Bunyinkgool <120672560+wavekanit@users.noreply.github.com>
2025-11-21 18:43:54 +0700

    feat: implement dag pipeline for etl process (#17)

---
+++ airflow/dags/dags_pipeline.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime, timedelta
+from pathlib import Path
+import airflow
+
+# ฟังก์ชันจะรันตอน task execution
+def get_minio_config():
+    return {
+        'MINIO_ACCESS_KEY': airflow.models.Variable.get("minio_access_key"),
+        'MINIO_SECRET_KEY': airflow.models.Variable.get("minio_secret_key"),
+        'MINIO_ENDPOINT': airflow.models.Variable.get("minio_endpoint"),
+        'MINIO_REGION': airflow.models.Variable.get("minio_region"),
+    }
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 2, 26),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "stockflow",
+    default_args=default_args,
+    schedule=None,
+    catchup=False,
+    tags=["stockflow"],
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+
+    # pull container image from ghcr.io and run the process
+    etl_container = DockerOperator(
+        task_id="run_etl_container",
+        image="ghcr.io/encall/stockflow/etl:latest",
+        api_version="auto",
+        auto_remove="success",
+        tty=True,
+        docker_url="unix://var/run/docker.sock",
+        environment=get_minio_config(),
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> etl_container >> end
